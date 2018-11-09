482a443df31b2c2788aa0b24e17ad05dfb9d271f
Alexander Dutton <alex.dutton@mysociety.org>
2018-11-09 11:02:52 +0000

    Add BASE_DIR setting

--- commons_api/settings.py
+++ commons_api/settings.py
@@ -1,5 +1,7 @@
 import os
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG'))
 USE_TZ = True
