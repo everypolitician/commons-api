0f85a3f35d088f92de54b69b775d0b81755e4902
Alexander Dutton <alex.dutton@mysociety.org>
2018-11-12 21:31:07 +0000

    Style and a homepage

--- commons_api/urls.py
+++ commons_api/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import RedirectView
+from django.views.generic import TemplateView
 from django.urls import path, include
 
 admin.autodiscover()
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='wikidata:country-list')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('', include('commons_api.wikidata.urls', 'wikidata')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
