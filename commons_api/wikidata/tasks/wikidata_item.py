6cf24fa2820116c0b6cb7f665dbb423c5d30579d
Alexander Dutton <alex.dutton@mysociety.org>
2018-11-13 09:14:07 +0000

    Factor out finding a Wikidata model from an app label and model name

--- commons_api/wikidata/tasks/wikidata_item.py
+++ commons_api/wikidata/tasks/wikidata_item.py
@@ -8,12 +8,17 @@ from commons_api.wikidata import models
 __all__ = ['refresh_from_entity_data']
 
 
-@celery.shared_task
-def refresh_from_entity_data(app_label, model, id):
+def get_wikidata_model(app_label, model):
     ct = ContentType.objects.get_by_natural_key(app_label, model)
     model = ct.model_class()
     if not issubclass(model, models.WikidataItem):
         raise TypeError('Model {} with content_type {}.{} is not a WikidataItem'.format(model, app_label, model))
+    return model
+
+
+@celery.shared_task
+def refresh_from_entity_data(app_label, model, id):
+    model = get_wikidata_model(app_label, model)
     obj = model.objects.get(id=id)  # type: models.WikidataItem
     response = requests.get('https://www.wikidata.org/wiki/Special:EntityData/{}.rdf'.format(id))
     graph = rdflib.ConjunctiveGraph()
