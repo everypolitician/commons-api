fe23c203ac7eb2d2184283dcb5cbc22135042728
GeoWill <will@mysociety.org>
2019-01-25 09:10:35 +0100

    Add signal to get geoshape on creation of spatial instance.

--- commons_api/wikidata/apps.py
+++ commons_api/wikidata/apps.py
@@ -13,6 +13,7 @@ class CountryConfig(apps.AppConfig):
                           sender=models.Country)
         post_save.connect(self.refresh_when_legislature_created,
                           sender=models.LegislativeHouse)
+        post_save.connect(self.refresh_geoshape_when_spatial_created)
 
     def refresh_legislatures_when_country_created(self, sender, instance, created, **kwargs):
         from commons_api.wikidata.tasks import refresh_legislatures
@@ -34,3 +35,15 @@ class CountryConfig(apps.AppConfig):
                                                          refresh_districts_last_queued=queued_at)
             refresh_members.delay(id=instance.id, queued_at=queued_at)
             refresh_districts.delay(id=instance.id, queued_at=queued_at)
+
+    def refresh_geoshape_when_spatial_created(self, sender, instance, created, **kwargs):
+        from commons_api.wikidata.tasks import refresh_geoshape
+        from commons_api.wikidata import models
+        from django.contrib.contenttypes.models import ContentType
+
+        if issubclass(sender, models.Spatial) and created and instance.geoshape_url and \
+                not instance.refresh_geoshape_last_queued:
+            queued_at = timezone.now()
+            sender.objects.filter(id=instance.id).update(refresh_geoshape_last_queued=queued_at)
+            ct = ContentType.objects.get_for_model(sender)
+            refresh_geoshape.delay(ct.app_label, ct.model, instance.id, queued_at=queued_at)
