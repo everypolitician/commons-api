717f578f0dc969b1462163b5ca84aea563853688
Alexander Dutton <alex.dutton@mysociety.org>
2019-01-30 17:47:26 +0000

    Add celery schedule config to update stuff regularly.

--- commons_api/celery.py
+++ commons_api/celery.py
@@ -10,3 +10,26 @@ app = Celery(__package__)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
+app.conf.beat_schedule = {
+    # Wholesale updates
+    'update-countries-daily': {
+        'task': 'commons_api.wikidata.tasks.country.refresh_country_list',
+        'schedule': 60 * 60 * 24,
+    },
+    'refresh-labels-periodically': {
+        'task': 'commons_api.wikidata.tasks.wikidata_item.refresh_labels_queue_periodically',
+        'schedule': 60,
+    },
+    'refresh-legislatures-periodically': {
+        'task': 'commons_api.wikidata.tasks.legislature.refresh_legislatures_queue_periodically',
+        'schedule': 60,
+    },
+    'refresh-members-periodically': {
+        'task': 'commons_api.wikidata.tasks.legislature.refresh_members_queue_periodically',
+        'schedule': 60,
+    },
+    'refresh-districts-periodically': {
+        'task': 'commons_api.wikidata.tasks.legislature.refresh_districts_queue_periodically',
+        'schedule': 60,
+    },
+}
