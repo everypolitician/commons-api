945d84caf864ec94d8ae2c09cee19c422b69fe64
Alexander Dutton <alex.dutton@mysociety.org>
2019-01-30 18:58:17 +0000

    Make sure that unittests don't attempt to access any networked resources

--- commons_api/tests/settings.py
+++ commons_api/tests/settings.py
@@ -1,3 +1,9 @@
 from ..settings import *
 
 SECRET_KEY = 'Acha9ohp2gae3chae7zeiwoo0Juhe2ooShu5eeP5wieL2ooxah1Aenie7Eim7cha'
+
+# https://stackoverflow.com/a/18601897
+import socket
+def guard(*args, **kwargs):
+    raise Exception("I told you not to use the Internet!")
+socket.socket = guard
