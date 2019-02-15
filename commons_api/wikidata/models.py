ed15f5989348ef008c42bbb4e396696b732e3f05
Alexander Dutton <alex.dutton@mysociety.org>
2019-02-15 09:04:37 +0000

    Add task to refresh metadata for a model from Wikidata

--- commons_api/wikidata/models.py
+++ commons_api/wikidata/models.py
@@ -1,3 +1,4 @@
+import collections
 from html import escape
 
 import rdflib
@@ -157,6 +158,32 @@ class WikidataItem(Moderateable, Timebound):
         return {language: {'href': self.wikipedia_article_links[language],
                            'title': self.wikipedia_article_titles[language]}
                 for language in self.wikipedia_article_links}
+
+    def update_from_metadata(self, rows, vars):
+        """Updates fields from SPARQL query results"""
+        field_values = collections.defaultdict(set)
+        for row in rows:
+            for name, binding in row.items():
+                if binding['type'] != 'bnode':
+                    field_values[name].add(binding['value'])
+        field_values.pop('id', None)
+        for var in vars:
+            values = field_values[var]
+            field = self._meta.get_field(name)
+            if not values:
+                setattr(self, name, None)
+            elif isinstance(field, (models.CharField, models.DateTimeField)):
+                setattr(self, name, values.pop())
+            elif isinstance(field, models.DateField):
+                # Wikidata provides dates as full timestamps, but we only want the 'YYYY-MM-DD' part.
+                setattr(self, name, values.pop()[:10])
+            elif isinstance(field, models.ForeignKey):
+                uri, label = values.pop(), field_values.get(name + 'Label', {''}).pop()
+                related = field.related_model.objects.for_id_and_label(item_uri_to_id(uri),
+                                                                       label, save=True)
+                setattr(self, name, related)
+            else:
+                raise ValueError("Unexpected field: {}".format(field))
 
     def __str__(self):
         return self.label
--- commons_api/wikidata/tasks/wikidata_item.py
+++ commons_api/wikidata/tasks/wikidata_item.py
@@ -1,12 +1,18 @@
 import datetime
+import logging
 
 import celery
 import itertools
 
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+
 from commons_api.wikidata import utils
 from commons_api.wikidata.tasks.base import with_periodic_queuing_task, get_wikidata_model_by_name
 
-__all__ = ['refresh_labels']
+__all__ = ['refresh_labels', 'refresh_metadata']
+
+logger = logging.getLogger(__name__)
 
 
 @with_periodic_queuing_task
@@ -31,3 +37,32 @@ def refresh_labels(self, app_label, model, ids=None, queued_at=None, rate_limiti
                                 for row in rows}
             items[id].save()
 
+
+@with_periodic_queuing_task
+@celery.shared_task(bind=True, queue='wdqs')
+@utils.queries_wikidata
+def refresh_metadata(self, app_label, model, queued_at=None, rate_limiting_handler=None):
+    template_name = f'{app_label}/query/{model}_metadata.rq'
+    try:
+        get_template(template_name)
+    except TemplateDoesNotExist:
+        return
+
+    queryset = get_wikidata_model_by_name(app_label, model).objects.all()
+    if queued_at is not None:
+        queryset = queryset.filter(refresh_metadata_last_queued=queued_at)
+    for items in utils.split_every(queryset, 250):
+        items = {item.id: item for item in items}
+        results = utils.templated_wikidata_query(template_name,
+                                                 {'ids': sorted(items)},
+                                                 rate_limiting_handler)
+        vars = {var for var in results['head']['vars'] if var != 'id' and not var.endswith('Label')}
+        for id, rows in itertools.groupby(results['results']['bindings'],
+                                          key=lambda row: row['id']['value']):
+            id = utils.item_uri_to_id(id)
+            rows = list(rows)
+            try:
+                items[id].update_from_metadata(rows, vars)
+                items[id].save()
+            except Exception:
+                logger.exception("Failed to update metadata for %s.%s %s (using %r)", app_label, model, id, rows)
